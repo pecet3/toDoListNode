f0b29e4c59ad5501f7aa2e7f797d6b4d214b9d92
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-21 23:51:16 +0100

    validation for add

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const handleCommand = ({ add, remove, list }) => {
     if (add) {
         if (typeof add !== 'string') {
             return console.log("zadanie musi być w formie tekstu!".red);
+        } else if (add.length < 7) {
+            return console.log("zadanie musi mieć przynajmniej 6 znaków!".red);
         }
         console.log("dodawanie");
     } else if (remove) {
