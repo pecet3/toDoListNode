44a8feaaeed761fcbbd2566f3b98d85eae1536d0
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-22 23:51:33 +0100

    edited handleData

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const parseArgs = require("minimist");
 const colors = require("colors");
+const fs = require("fs");
+
+
 const command = parseArgs(process.argv.slice(2, 3));
 delete command._
 console.log(command);
@@ -12,14 +15,49 @@ const handleCommand = ({ add, remove, list }) => {
         } else if (add.length < 7) {
             return console.log("zadanie musi mieć przynajmniej 6 znaków!".red);
         }
-        console.log("dodawanie");
+        handleData(1, add);
     } else if (remove) {
-        console.log("usuwanie");
-    } else if (list) {
-        console.log("lista");
+        if (typeof remove !== 'string' || remove.length < 7) {
+            return console.log("podano błędną nazwę zadania...".red);
+        }
+        handleData(2, remove);
+    } else if (list || list === "") {
+        handleData(3, null);
     } else {
-        console.log("nie rozumiem polecenia");
+        console.log("Nie rozumiem polecenia. Uzyj:\n--add, aby dodać zadanie\n--remove, żeby usunąć zadanie ".cyan);
     }
 }
+
+const handleData = (type, title) => {
+    // type - number(1 - add, 2 - remove, 3 - list)
+    // title (string || null)
+
+    const data = fs.readFileSync("data.json", "utf-8");
+    const tasks = JSON.parse(data);
+    console.log(tasks);
+
+    if (type === 1 || type === 2) {
+        const isExisted = tasks.find(task => task.title === title) ? true : false;
+        if (type === 1 && isExisted) {
+            return console.log("takie zadanie już istnieje!".red);
+        }
+        else if (type === 2 && !isExisted) {
+            return console.log("takie zadanie nie istnieje!".red);
+        }
+    }
+
+    switch (type) {
+        case 1:
+            console.log("Dodaje zadanie...".cyan);
+            break;
+        case 2:
+            console.log("Usuwam zadanie...".cyan);
+            break;
+        case 3:
+            console.log("Wyświetlam listę...".cyan);
+            break;
+
+    }
+};
 
 handleCommand(command);
